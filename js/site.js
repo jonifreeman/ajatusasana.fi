b39f6af4a57a7f829b19e3a5ec3a91bebbbd463c
Joni Freeman <joni.freeman@reaktor.fi>
2014-07-27 16:07:34 +0300

    Add validation

--- js/site.js
+++ js/site.js
@@ -13,6 +13,7 @@ $(function() {
   })
 
   $('.signup').click(function(e) {
+    validate()
     var time = $(e.currentTarget).parent('div').find('strong').text()
     var course = $(e.currentTarget).next().text()
     $('.signup-popup .course').text(time + " " + course)
@@ -20,11 +21,38 @@ $(function() {
     $('.signup-popup').css('visibility', 'visible')
   })
 
+  function name() { return $('.signup-popup .name').val() }
+  function contact() { return $('.signup-popup .contact').val() }
+
+  function validate() {
+    if (name().length == 0 || contact().length == 0) {
+      $('.signup-popup .signup-button').attr('disabled', 'disabled')
+    } else {
+      $('.signup-popup .signup-button').removeAttr('disabled')
+    }
+
+    if (name().length == 0) {
+      $('.signup-popup .name').addClass('invalid')
+    } else {
+      $('.signup-popup .name').removeClass('invalid')
+    }
+
+    if (contact().length == 0) {
+      $('.signup-popup .contact').addClass('invalid')
+    } else {
+      $('.signup-popup .contact').removeClass('invalid')
+    }
+  }
+
+  $('.signup-popup input').bind("keyup blur", function(event) {
+    validate()
+  })
+
   $('.signup-popup .signup-button').click(function(e) {
     data = {
       course:  $('.signup-popup .course').text(),
-      name:    $('.signup-popup .name').val(),
-      contact: $('.signup-popup .contact').val()
+      name:    name(),
+      contact: contact()
     }
     $.post("signup.php", data, function() {
       $('.signup-popup').css('visibility', 'hidden')
