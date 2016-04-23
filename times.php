85dc74e19885e8d891b150f85d52f2a227155bce
Joni Freeman <joni.freeman@reaktor.fi>
2016-04-23 17:58:22 +0300

    Add dates to queries

--- times.php
+++ times.php
@@ -1,38 +1,21 @@
 <?php
 
 // TODO include ('common.php');
-function internalServerError($msg) {
-  echo $msg;
-  var_dump(http_response_code(500));
-  die($msg);
-}
-
-function sql_query($sql, $params, $query_handler = 'all_rows_handler') {
+function sql_query($sql) {
   $db_server = "127.0.0.1";
   $db_database = "ajatusas";
   $db_username = "ajatusas_user";
   $db_password = "secret";
   $conn = mysqli_connect($db_server, $db_username, $db_password, $db_database);
   if (!$conn) {
-    internalServerError("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
   }
-  $stmt = mysqli_prepare($conn, $sql);
-  foreach ($params as $param) {
-    mysqli_stmt_bind_param($stmt, $param, $params[$param]);
-  }
-  mysqli_stmt_execute($stmt) or internalServerError(mysqli_stmt_error($stmt));
-  $query_result = mysqli_stmt_get_result($stmt);
-  $result = $query_handler($query_result);
-  mysqli_stmt_close($stmt);
-  mysqli_close($conn);
-  return $result;
-}
-
-function all_rows_handler($results) {
+  $query_result = mysqli_query($conn, $sql($conn)) or die(mysqli_error());
   $rows = array();
-  while ($row = mysqli_fetch_assoc($results)) {
+  while ($row = mysqli_fetch_assoc($query_result)) {
     array_push($rows, $row);
   }
+  mysqli_close($conn);
   return $rows;
 }
 
@@ -46,10 +29,18 @@ function get_vacant_times($start, $end) {
 function get_times_and_enrollments($start, $end) {
   verify_auth_token();
   // TODO add where
-  $times_sql = "SELECT *, 'time' AS type FROM times WHERE start>=? AND end <= ?";
-  $times = sql_query($times_sql, array('start' => $start, 'end' => $end));
-  $enrollments_sql = "SELECT *, 'enrollment' AS type FROM enrollments WHERE start>=? AND end <= ?";
-  $enrollments = sql_query($enrollments_sql, array('start' => $start, 'end' => $end));
+  $times_sql = function($conn) use ($start, $end) {
+    $s = mysqli_real_escape_string($conn, $start);
+    $e = mysqli_real_escape_string($conn, $end);
+    return "SELECT *, 'time' AS type FROM times WHERE start >= '$s' AND end <= '$e'";
+  };
+  $times = sql_query($times_sql);
+  $enrollments_sql = function($conn)  use ($start, $end) {
+    $s = mysqli_real_escape_string($conn, $start);
+    $e = mysqli_real_escape_string($conn, $end);
+    return "SELECT *, 'enrollment' AS type FROM enrollments WHERE start >= '$s' AND end <= '$e'";
+  };
+  $enrollments = sql_query($enrollments_sql);
   $result_json = json_encode(array_merge($times, $enrollments));
   echo $result_json;
 }
