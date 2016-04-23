04e42843a43243f542146e9e785b106ead5449eb
Joni Freeman <joni.freeman@reaktor.fi>
2016-04-23 18:41:42 +0300

    Add create time API

--- times.php
+++ times.php
@@ -1,11 +1,14 @@
 <?php
 
+$db_server = "127.0.0.1";
+$db_database = "ajatusas";
+$db_username = "ajatusas_user";
+$db_password = "secret";
+
 // TODO include ('common.php');
 function sql_query($sql) {
-  $db_server = "127.0.0.1";
-  $db_database = "ajatusas";
-  $db_username = "ajatusas_user";
-  $db_password = "secret";
+  global $db_server, $db_database, $db_username, $db_password;
+  
   $conn = mysqli_connect($db_server, $db_username, $db_password, $db_database);
   if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
@@ -17,6 +20,18 @@ function sql_query($sql) {
   }
   mysqli_close($conn);
   return $rows;
+}
+
+function sql_update($sql) {
+  global $db_server, $db_database, $db_username, $db_password;
+  
+  $conn = mysqli_connect($db_server, $db_username, $db_password, $db_database);
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  $result = mysqli_query($conn, $sql($conn)) or die(mysqli_error());
+  mysqli_close($conn);
+  return $result->affected_rows;
 }
 
 function verify_auth_token() {
@@ -35,7 +50,7 @@ function get_times_and_enrollments($start, $end) {
     return "SELECT *, 'time' AS type FROM times WHERE start >= '$s' AND end <= '$e'";
   };
   $times = sql_query($times_sql);
-  $enrollments_sql = function($conn)  use ($start, $end) {
+  $enrollments_sql = function($conn) use ($start, $end) {
     $s = mysqli_real_escape_string($conn, $start);
     $e = mysqli_real_escape_string($conn, $end);
     return "SELECT *, 'enrollment' AS type FROM enrollments WHERE start >= '$s' AND end <= '$e'";
@@ -54,6 +69,23 @@ function get_times() {
   } else {
     get_times_and_enrollments($start, $end);
   }
+}
+
+function create_time($start, $end) {
+  verify_auth_token();
+  $sql = function($conn) use ($start, $end) {
+    $s = mysqli_real_escape_string($conn, $start);
+    $e = mysqli_real_escape_string($conn, $end);
+    return "INSERT INTO times(start, end) VALUES ('$s', '$e')";
+  };
+  sql_update($sql);
+}
+
+function create_or_update_times() {
+  // TODO: add support for update: $_POST['id']
+  $start = $_POST['start'];
+  $end = $_POST['end'];
+  create_time($start, $end);  
 }
 
 $method = $_SERVER['REQUEST_METHOD'];
