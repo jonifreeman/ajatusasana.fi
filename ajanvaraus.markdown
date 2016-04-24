fe0a2dc36ac7938f7d145fce869c40b100051e74
Joni Freeman <joni.freeman@reaktor.fi>
2016-04-24 10:38:27 +0300

    New time slots can be created

--- ajanvaraus.markdown
+++ ajanvaraus.markdown
@@ -2,5 +2,16 @@
 title: Ajanvaraus
 ---
 
+<div class="popup add-appointment-time-popup">
+  <div class="popup-content">
+   <img class="close" src="/img/popup_close.png" />
+   <h2>Uusi vapaa aika</h2>
+   <div class="contact-info">Alku (esim. 12:00): </div><input type="text" class="start" />
+   <div class="contact-info">Loppu (esim. 16:30): </div><input type="text" class="end" />
+   <div class="error">Ajan lisäys epäonnistui. Yritä hetken kuluttua uudestaan.</div>
+   <input class="add-appointment-time-button" type="button" value="Lisää aika" />
+  </div>
+</div>
+
 <div id='calendar'></div>
 
--- js/ajanvaraus.js
+++ js/ajanvaraus.js
@@ -1,5 +1,7 @@
 $(function() {
 
+  var addTimePopup = setupAddTimePopup()
+
   $('#calendar').fullCalendar({
     lang: 'fi',
     events: 'times.php',
@@ -9,9 +11,6 @@ $(function() {
     allDaySlot: false,
     height: 'auto',
     eventDataTransform: function(event) {
-      if (event.className == 'time') {
-        event.rendering = 'background'
-      }
       return event
     },
     eventRender: function(event, element) {
@@ -20,8 +19,72 @@ $(function() {
           content: '<p>' + event.comment + '</p><p>' + event.email + '</p><p>' + event.phone + '</p>'
         })
       }
+    },
+    dayClick: function(date, jsEvent, view) {
+      addTimePopup.open(jsEvent, {
+        date: date,
+        onSuccess: function() {
+          $('#calendar').fullCalendar('refetchEvents')
+        }
+      })
+    },
+    eventClick: function(calEvent, jsEvent, view) {
+      console.log(calEvent)
     }
   })
 
-
 })
+
+function setupAddTimePopup() {
+  function validate() {
+  }
+
+  var $container = $('.add-appointment-time-popup')
+
+  function start() { return $container.find('.start').val() }
+  function end() { return $container.find('.end').val() }
+
+  $container.find('.add-appointment-time-button').click(function(e) {
+    var containerData = $container.data().data
+    var date = containerData.date
+    var s = date.clone().time(start())
+    var e = date.clone().time(end())
+    data = {
+      start: s.format(),
+      end: e.format()
+    }
+    $.post("times.php", data, function() {
+      $container.css('visibility', 'hidden')
+      if (containerData.onSuccess)
+        containerData.onSuccess()
+    })
+    .fail(function() {
+      $container.find('.error').fadeIn(500).delay(10000).fadeOut(500)
+    })
+  })
+
+  return setupPopup($container, validate)
+}
+
+function setupPopup($container, validate) {
+  var open = function(e, containerData) {
+    $container.data('data', containerData)
+    setTimeout(function() {
+      $container.find('input:nth(0)').focus()
+    }, 0)
+    validate()
+    $('.popup').css('visibility', 'hidden')
+    $container.offset($(e.currentTarget).offset())
+    $container.css('visibility', 'visible')
+  }
+
+  $container.find('input').bind("keyup blur", function(event) {
+    validate()
+  })
+
+  $container.find('.close').click(function() {
+    $container.css('visibility', 'hidden')
+  })
+
+  return {open: open}
+}
