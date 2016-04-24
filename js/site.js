2eba83b1cd5c49516e9aa72450b89b16754fe143
Joni Freeman <joni.freeman@reaktor.fi>
2016-04-24 20:22:06 +0300

    Improve popup positioning

--- js/site.js
+++ js/site.js
@@ -38,13 +38,17 @@ function setupSignup() {
     var time = $(e.currentTarget).parent('div').find('strong').text()
     var course = $(e.currentTarget).next().text()
     $('.signup-popup .course').text(time + " " + course)
-    $('.signup-popup').offset($(e.currentTarget).offset())
+    var offset = $(e.currentTarget).offset()
+    offset.left = Math.max(0, offset.left - 200)
+    $('.signup-popup').offset(offset)
     $('.signup-popup').css('visibility', 'visible')
   })
 
   $('.schedule .cancelled').click(function(e) {
     $('.popup').css('visibility', 'hidden')
-    $('.cancelled-popup').offset($(e.currentTarget).offset())
+    var offset = $(e.currentTarget).offset()
+    offset.left = Math.max(0, offset.left - 200)
+    $('.cancelled-popup').offset(offset)
     $('.cancelled-popup').css('visibility', 'visible')
   })
 
