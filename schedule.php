d8c56001ec6c50d18fcec60f9a0fbcf11afd3401
Joni Freeman <joni.freeman@reaktor.fi>
2016-08-06 21:45:13 +0300

    Add miniretreat

--- schedule.php
+++ schedule.php
@@ -2,20 +2,26 @@
 
 include ('common.php');
 
-// TODO use start + end
 function query_group_classes() {
   $sql = function($conn) {
-    return "SELECT *, HOUR(start_time) as start_time_hour, 'session' as 'session_type' FROM group_class WHERE is_saturday_miniretreat is false";
+    return "SELECT *, HOUR(start_time) as start_time_hour, 'session' as 'session_type' FROM group_class WHERE NOW() >= start AND (NOW() < end OR end IS NULL) AND is_saturday_miniretreat is false";
   };
   return sql_query($sql);
 }
 
-// TODO add miniretreat placeholder
+$miniretreat = array(
+  "name" => "Ajatus & Asana miniretriitit ja teemapäivät kerran kuussa",
+  "day" => "la",
+  "anchor" => "retriitti",
+  "start_time_hour" => 11,
+  "session_type" => "course"
+);
 
 $weekdays = array('ma', 'ti', 'ke', 'to', 'pe', 'la', 'su');
 // create array of arrays binned by start time: 8, 11, 14, 18
 $bins = array(18, 14, 11, 8);
 $all_group_classes = query_group_classes();
+array_push($all_group_classes, $miniretreat);
 $group_classes_bins = array();
 foreach ($bins as $bin) {
   $bin_array = array();
@@ -27,6 +33,8 @@ foreach ($bins as $bin) {
   }
   array_unshift($group_classes_bins, $bin_array);
 }
+
+header('Content-Type: text/html; charset=utf-8');
 ?>
 
 
@@ -45,19 +53,22 @@ foreach ($bins as $bin) {
 </thead>
 
 <tbody>
-<?php foreach ($weekdays as $day): ?>
-<tr>
 <?php foreach ($group_classes_bins as $group_classes): ?>
-<?php foreach ($group_classes as $group_class): ?>
+<tr>
+<?php foreach ($weekdays as $day): ?>
  <td>
+<?php foreach ($group_classes as $group_class): ?>
 <?php if ($group_class['day'] == $day): ?>
  <div class="<?= $group_class['session_type'] ?>">
-  <strong><?= $group_class['start_time'] ?> - <?= $group_class['end_time'] ?></strong> <br /> <img class="signup" src="/img/signup.png"></img>
+  <?php if ($group_class['start_time']): ?>
+  <strong><?= $group_class['start_time'] ?> - <?= $group_class['end_time'] ?></strong> <br />
+  <? endif; ?>
+  <img class="signup" src="/img/signup.png"></img>
   <a href="#<?= $group_class['anchor'] ?>"><?= $group_class['name'] ?></a>
  </div>
 <?php endif; ?>
- </td>
 <?php endforeach; ?>
+ </td>
 <?php endforeach; ?>
 </tr>
 <?php endforeach; ?>
