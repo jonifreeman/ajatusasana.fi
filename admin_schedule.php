2aa0e3d52caf5b188dbddeff4dfed0bb630b73fd
Joni Freeman <joni.freeman@reaktor.fi>
2016-08-15 21:31:15 +0300

    Initial form

---
+++ admin_schedule.php
@@ -0,0 +1,57 @@
+<?php
+
+include ('common.php');
+
+function query_open_group_classes() {
+  $sql = function($conn) {
+    return "SELECT gc.*, GROUP_CONCAT(rc.email ORDER BY rc.group_class_id) AS regulars FROM group_class gc LEFT JOIN regular_client rc ON gc.id=rc.group_class_id WHERE NOW() < gc.end OR gc.end IS NULL GROUP BY gc.id";
+  };
+  return sql_query($sql);
+}
+
+$group_classes = query_open_group_classes();
+
+// TODO access control
+
+?>
+
+<table>
+  <thead>
+    <tr>
+      <th>Nimi</th>
+      <th>Viikonpäivä</th>
+      <th>Kello</th>
+      <th>Alkaa</th>
+      <th>Päättyy</th>
+      <th>Näkyy kalenterissa alkaen</th>
+      <th>Max</th>
+      <th>Huom!</th>
+      <th>Linkki</th>
+      <th>Vakiokävijät</th>
+      <th>Tyyppi</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <?php foreach ($group_classes as $group_class): ?>
+    <tr>
+      <td><input type="text" name="name" value="<?= $group_class['name'] ?>"/></td>
+      <td><input type="text" name="day" value="<?= $group_class['day'] ?>"/></td>
+      <td><input type="text" name="start_time" value="<?= $group_class['start_time'] ?>"/> - <input type="text" name="end_time" value="<?= $group_class['end_time'] ?>"/</td>
+      <td><input type="text" name="start" value="<?= $group_class['start'] ?>"/></td>
+      <td><input type="text" name="end" value="<?= $group_class['end'] ?>"/></td>
+      <td><input type="text" name="display_start" value="<?= $group_class['display_start'] ?>"/></td>
+      <td><input type="text" name="max_size" value="<?= $group_class['max_size'] ?>"/></td>
+      <td><input type="text" name="highlight" value="<?= $group_class['highlight'] ?>"/></td>
+      <td><input type="text" name="anchor" value="<?= $group_class['anchor'] ?>"/></td>
+      <td><textarea name="name"><?= $group_class['regulars'] ?></textarea></td>
+      <td>
+        <input type="radio" name="type" value="normal" <?= $group_class['type'] == 'normal' ? 'checked' : '' ?>>Normaali</input>
+        <input type="radio" name="type" value="miniretreat" <?= $group_class['type'] == 'miniretreat' ? 'checked' : '' ?>>Miniretriitti</input>
+        <input type="radio" name="type" value="course" <?= $group_class['course'] == 'normal' ? 'checked' : '' ?>>Kurssi</input>
+      </td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+
+</table>
