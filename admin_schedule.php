a9550c6823eb2a5dbacc4882b8aa80c73f30e54f
Joni Freeman <joni.freeman@reaktor.fi>
2016-08-22 21:00:57 +0300

    Use date picker

--- admin_schedule.php
+++ admin_schedule.php
@@ -90,9 +90,9 @@ header('Content-Type: text/html; charset=utf-8');
       <td><input type="text" name="name" value="<?= $group_class['name'] ?>"/></td>
       <td><input type="text" name="day" value="<?= $group_class['day'] ?>"/></td>
       <td><input type="text" name="start_time" value="<?= format_timestring($group_class['start_time']) ?>"/> - <input type="text" name="end_time" value="<?= format_timestring($group_class['end_time']) ?>"/</td>
-      <td><input type="text" name="start" value="<?= $group_class['start'] ?>"/></td>
-      <td><input type="text" name="end" value="<?= $group_class['end'] ?>"/></td>
-      <td><input type="text" name="display_start" value="<?= $group_class['display_start'] ?>"/></td>
+      <td><input type="date" name="start" value="<?= $group_class['start'] ?>"/></td>
+      <td><input type="date" name="end" value="<?= $group_class['end'] ?>"/></td>
+      <td><input type="date" name="display_start" value="<?= $group_class['display_start'] ?>"/></td>
       <td><input type="text" name="max_size" value="<?= $group_class['max_size'] ?>"/></td>
       <td><input type="text" name="highlight" value="<?= $group_class['highlight'] ?>"/></td>
       <td><input type="text" name="anchor" value="<?= $group_class['anchor'] ?>"/></td>
@@ -115,8 +115,6 @@ header('Content-Type: text/html; charset=utf-8');
   mon, tue, wed, thu, fri, sat, sun
   <li><b>Kello</b></li>
   Esim. 12:30
-  <li><b>Päivämäärät</b></li>
-  Formaatissa vuosi-kk-pv, esim 2017-08-14
   <li><b>Kalenterissa</b></li>
   Päivämäärä milloin ilmestyy näkyville kalenteriin. Eli voi laittaa kalenteriin näkyville ennen kuin tunnit varsinaisesti alkavat.
   <li><b>Max</b></li>
