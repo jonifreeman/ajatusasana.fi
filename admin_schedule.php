e8173acaf62bf4cabda85f4b0a131c206bad935e
Joni Freeman <joni.freeman@reaktor.fi>
2016-08-27 20:04:41 +0300

    Tweak layout

--- admin_schedule.php
+++ admin_schedule.php
@@ -81,8 +81,7 @@ if (check_auth_token() == FALSE) {
       <th>Nimi</th>
       <th>Pv</th>
       <th>Kello</th>
-      <th>Alkaa</th>
-      <th>Päättyy</th>
+      <th>Alkaa/Päättyy</th>
       <th>Kalenterissa</th>
       <th>Max</th>
       <th>Huom!</th>
@@ -101,9 +100,8 @@ if (check_auth_token() == FALSE) {
       </td>
       <td><input type="text" name="name" value="<?= $group_class['name'] ?>"/></td>
       <td><input type="text" name="day" value="<?= $group_class['day'] ?>"/></td>
-      <td><input type="text" name="start_time" value="<?= format_timestring($group_class['start_time']) ?>"/> - <input type="text" name="end_time" value="<?= format_timestring($group_class['end_time']) ?>"/</td>
-      <td><input type="date" name="start" value="<?= $group_class['start'] ?>"/></td>
-      <td><input type="date" name="end" value="<?= $group_class['end'] ?>"/></td>
+      <td><input type="text" name="start_time" value="<?= format_timestring($group_class['start_time']) ?>"/> - <input type="text" name="end_time" value="<?= format_timestring($group_class['end_time']) ?>"/></td>
+      <td><input type="date" name="start" value="<?= $group_class['start'] ?>"/> - <input type="date" name="end" value="<?= $group_class['end'] ?>"/></td>
       <td><input type="date" name="display_start" value="<?= $group_class['display_start'] ?>"/></td>
       <td><input type="text" name="max_size" value="<?= $group_class['max_size'] ?>"/></td>
       <td><input type="text" name="highlight" value="<?= $group_class['highlight'] ?>"/></td>
